924ac778aa3877ddfccdaa2d689bb9315957eda1
Ioannis Mousmoutis <ioannis.mousmoutis@eurodyn.com>
2019-02-14 17:16:05 +0200

    Added more UserService tests

--- src/main/java/com/eurodyn/qlack/test/QlackSpringBootConsoleApplication.java
+++ src/main/java/com/eurodyn/qlack/test/QlackSpringBootConsoleApplication.java
@@ -8,8 +8,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 @SpringBootApplication
 @EnableCaching
@@ -19,13 +21,13 @@ import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
         "com.eurodyn.qlack.test.services",
         "com.eurodyn.qlack.fuse.aaa"
 })
-public class SpringBootConsoleApplication implements CommandLineRunner {
+public class QlackSpringBootConsoleApplication implements CommandLineRunner {
 
     @Autowired
     private UserTestService userTestService;
 
     public static void main(String[] args) {
-        SpringApplication app = new SpringApplication(SpringBootConsoleApplication.class);
+        SpringApplication app = new SpringApplication(QlackSpringBootConsoleApplication.class);
         app.setBannerMode(Banner.Mode.OFF);
         app.run(args);
     }
@@ -39,9 +41,17 @@ public class SpringBootConsoleApplication implements CommandLineRunner {
             switch (args[0]){
                 case "UserService":
                     userTestService.createUser();
+                    userTestService.updateUser();
+                    userTestService.getUserByName();
+                    userTestService.deleteUser();
                     break;
                 default: System.out.println("Service " +args[0]+ " is not found :(");
             }
         }
+    }
+
+    @Bean
+    public BCryptPasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
     }
 }
--- src/main/java/com/eurodyn/qlack/test/services/aaa/UserTestService.java
+++ src/main/java/com/eurodyn/qlack/test/services/aaa/UserTestService.java
@@ -1,16 +1,24 @@
 package com.eurodyn.qlack.test.services.aaa;
 
+import com.eurodyn.qlack.fuse.aaa.dto.UserAttributeDTO;
 import com.eurodyn.qlack.fuse.aaa.dto.UserDTO;
+import com.eurodyn.qlack.fuse.aaa.model.UserAttribute;
 import com.eurodyn.qlack.fuse.aaa.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
 import java.util.HashSet;
+import java.util.List;
 
 @Service
 public class UserTestService {
 
     private final UserService userService;
+
+    private final String USERNAME = "ioannis.mousmoutis";
+
+    private final UserDTO userDTO = createUserDTO();
 
     @Autowired
     public UserTestService(UserService userServiceService) {
@@ -18,12 +26,101 @@ public class UserTestService {
     }
 
     public void createUser(){
-        UserDTO user = new UserDTO();
-        user.setUsername("imousmoutis");
-        user.setPassword("imousmoutis");
-        user.setStatus((byte) 1);
-        user.setUserAttributes(new HashSet<>());
-        userService.createUser(user);
+        System.out.println("******************");
+        System.out.println("Testing createUser method.");
+        UserDTO existingUser = userService.getUserByName(USERNAME);
+
+        if (existingUser == null){
+            String userId = userService.createUser(userDTO);
+            System.out.println("User with id " +userId+ " has been created.");
+        } else {
+            System.out.println("User " +USERNAME+ " already exists.");
+        }
+        System.out.println("******************");
     }
 
+    public void updateUser(){
+        System.out.println("******************");
+        System.out.println("Testing updateUser method.");
+        UserDTO existingUser = userService.getUserByName(USERNAME);
+
+        existingUser.setSuperadmin(false);
+        for (UserAttributeDTO u: existingUser.getUserAttributes()){
+            u.setData("updated " +u.getData());
+        }
+        if(existingUser.getUserAttributes().size() == 2){
+            UserAttributeDTO userAttributeDTO = new UserAttributeDTO();
+            userAttributeDTO.setName("email");
+            userAttributeDTO.setData("ioannis.mousmoutis@eurodyn.com");
+            userAttributeDTO.setUserId(existingUser.getId());
+            existingUser.getUserAttributes().add(userAttributeDTO);
+        }
+
+        userService.updateUser(existingUser, false, true);
+        System.out.println("User is updated.");
+        System.out.println("******************");
+    }
+
+    public void deleteUser(){
+        System.out.println("******************");
+        System.out.println("Testing deleteUser method.");
+
+        UserDTO existingUser = userService.getUserByName(USERNAME);
+        userService.deleteUser(existingUser.getId());
+
+        System.out.println("User is deleted.");
+        System.out.println("******************");
+    }
+
+    private void getUserById(String userId){
+        System.out.println("******************");
+        System.out.println("Testing getUserById method.");
+
+        UserDTO existingUser = userService.getUserById(userId);
+
+        System.out.println("Found user is " +existingUser.getUsername());
+        System.out.println("******************");
+    }
+
+    public void getUserByName(){
+        System.out.println("******************");
+        System.out.println("Testing getUserByName method.");
+
+        UserDTO existingUser = userService.getUserByName(USERNAME);
+
+        System.out.println("Found user is " +existingUser.getUsername());
+
+        System.out.println("******************");
+        getUserById(existingUser.getId());
+    }
+
+    private List<UserAttributeDTO> createUserAttributesDTO(){
+        List<UserAttributeDTO> userAttributesDTO = new ArrayList<>();
+
+        UserAttributeDTO userAttributeDTO = new UserAttributeDTO();
+        userAttributeDTO.setName("fullName");
+        userAttributeDTO.setData("Ioannis Mousmoutis");
+        userAttributeDTO.setContentType("text");
+        userAttributesDTO.add(userAttributeDTO);
+
+        UserAttributeDTO userAttributeDTO2 = new UserAttributeDTO();
+        userAttributeDTO2.setName("company");
+        userAttributeDTO2.setData("European Dynamics");
+        userAttributeDTO2.setContentType("text");
+        userAttributesDTO.add(userAttributeDTO2);
+
+        return userAttributesDTO;
+    }
+
+    private UserDTO createUserDTO(){
+        UserDTO userDTO = new UserDTO();
+        userDTO.setUsername(USERNAME);
+        userDTO.setPassword("thisisaverysecurepassword");
+        userDTO.setStatus((byte)1);
+        userDTO.setSuperadmin(true);
+        userDTO.setExternal(false);
+        userDTO.setUserAttributes(new HashSet<>(createUserAttributesDTO()));
+
+        return userDTO;
+    }
 }
