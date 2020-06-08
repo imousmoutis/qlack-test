a78f0071305ece52ead42150d041641fdfbfc28f
kliarist <theodoros.kliaris@gmail.com>
2020-06-08 12:10:03 +0300

    Get rid of not using generic warning

--- src/main/java/com/eurodyn/qlack/test/cmd/repository/AccountRepository.java
+++ src/main/java/com/eurodyn/qlack/test/cmd/repository/AccountRepository.java
@@ -1,14 +1,13 @@
 package com.eurodyn.qlack.test.cmd.repository;
 
 import com.eurodyn.qlack.test.cmd.model.Account;
-import java.io.Serializable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.querydsl.QuerydslPredicateExecutor;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface AccountRepository<T extends Account, I extends Serializable> extends
-    JpaRepository<T, I>, QuerydslPredicateExecutor<T> {
+public interface AccountRepository extends JpaRepository<Account, String>,
+    QuerydslPredicateExecutor<Account> {
 
 }
 
